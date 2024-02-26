9374f8c56c3271ca7775e09aa3a769c6219322f7
radek <rajkonkret660@gmail.com>
2024-02-26 11:01:51 +0100

    1 - pierwszy string

---
+++ pierwszy.py
@@ -0,0 +1,12 @@
+print()  # wydrukuj/wypisz
+# ctrl alt l - formatowanie programu wg zasad PEP8
+print("Radek")
+# ctrl - d  kopiowanie lini
+print("Radek")
+print('Nazywam się Radek')
+print('Nazywam się Radek')
+print('Nazywam się Radek')
+print('Nazywam się Radek')
+# type() - wypisanie typu danej
+print(type("Radek"))  # <class 'str'>
+# 11:30
