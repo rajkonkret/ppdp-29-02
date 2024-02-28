67070e679d945a664b4c63fe07d0f5c74d7d90e3
radek <rajkonkret660@gmail.com>
2024-02-28 11:07:33 +0100

    3 - add fun11 dek

---
+++ fun11-dek2.py
@@ -0,0 +1,24 @@
+def uppercase_decorator(func):
+    def wrapper():
+        result = func()
+        return result.upper()
+
+    return wrapper
+
+
+def bold_decorator(func):
+    def wrapper():
+        result = func()
+        return "\033[1m" + result + "\033[0m"
+
+    return wrapper
+
+
+@bold_decorator
+@uppercase_decorator
+def greeting():
+    return "Hello World!"
+
+
+# dekorator, ktory zmienia wynik tej funkcji  na duze litery
+print(greeting())  # HELLO WORLD!
