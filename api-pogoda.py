d2d561201bdaaa040bb1612340dc0f6effd41d3b
radek <rajkonkret660@gmail.com>
2024-02-29 14:20:22 +0100

    4 - update api-pogoda

--- api-pogoda.py
+++ api-pogoda.py
@@ -1,4 +1,10 @@
 import requests
-url =     'http://api.openweathermap.org/data/2.5/'
-    'weather?q=Warszawa&appid=99a24a78addf4a2c41947189fcff67f7&&lang=p&format=jsonl&units=metric'
-page = requests.get()
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=99a24a78addf4a2c41947189fcff67f7&&lang=p&format=jsonl&units=metric'
+page = requests.get(url)
+print(page.text)
+json = page.json()
+print(json)  # 8.83
+temp = json['main']['temp']
+print(temp)  # 8.83
+
